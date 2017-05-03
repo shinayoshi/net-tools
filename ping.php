7c56133f0569d3e7d6467942e9f0616eda0791a5
shinayoshi <ryota+git@shinayoshi.net>
2017-05-04 01:59:51 +0900

    add ping.php

--- ping.php
+++ ping.php
@@ -1,13 +1,16 @@
+<?php
+$cmd="/sbin/ping -c 4 localhost";
+$res=shell_exec($cmd);
+?>
 <!DOCTYPE html>
 <html lang="ja">
   <head>
     <meta charset="utf-8" />
-    <title>net-tools</title>
+    <title>ping - net-tools</title>
     <meta name="description" content="shinayoshi shinayoshi.net" />
     <meta name="author" content="shinayoshi" />
   </head>
   <body>
-    <p><?php echo $_SERVER["REMOTE_ADDR"]; ?></p>
-    <p><a href="ping.php">ping</a></p>
+    <p><?php var_dump($res); ?></p>
   </body>
 </html>
