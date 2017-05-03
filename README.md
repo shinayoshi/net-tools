0553520abe106b4cdb54ff0688e457374351954e
shinayoshi <ryota+git@shinayoshi.net>
2017-05-03 23:37:55 +0900

    Initial commit.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # net-tools
+
+This repository is Web application of network tools.
+
+## License
+
+This theme is released under the MIT license, see [LICENSE.md](LICENSE.md).
+
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8" />
+    <title>net-tools</title>
+    <meta name="description" content="shinayoshi shinayoshi.net" />
+    <meta name="author" content="shinayoshi" />
+  </head>
+  <body>
+    <p><?php echo $_SERVER["REMOTE_ADDR"]; ?></p>
+  </body>
+</html>
