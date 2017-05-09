0d5f3dc177a0effabcbdcb6bc9a2e4b49faf5ff2
shinayoshi <ryota+git@shinayoshi.net>
2017-05-09 23:58:16 +0900

    update command.php

--- command.php
+++ command.php
@@ -7,13 +7,21 @@ session_start();
 
 $ini_file = './configs/net-tools.ini';
 $captcha_auth = $_SESSION['captcha_auth'];
+$nowtime = strtotime('now');
+$last_exec_time = $_SESSION['last_exec_time'];
 $command = htmlspecialchars($_POST['command']);
 $hostname = htmlspecialchars($_POST['hostname']);
 
 if ($captcha_auth) {
-    $ntu = new NetToolsUtil($ini_file);
-    $result = $ntu->execute($command, $hostname);
-    echo json_encode($result);
+    if (!isset($last_exec_time) || $nowtime - $last_exec_time > 1) {
+        $_SESSION['last_exec_time'] = $nowtime;
+        $ntu = new NetToolsUtil($ini_file);
+        $result = $ntu->execute($command, $hostname);
+        echo json_encode($result);
+    } else {
+        $result = array('command'=>'', 'result'=>'Command exec interval is too short.');
+        echo json_encode($result);
+    }
 } else {
     $result = array('command'=>'', 'result'=>'CAPTCHA authentication is not done.');
     echo json_encode($result);
