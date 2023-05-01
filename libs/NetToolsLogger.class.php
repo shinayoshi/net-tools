f0f2c9bf4a386a36e278279d84e65c9b615f0402
Ryota SHINADA <ryota+github@shinayoshi.net>
2023-05-01 12:50:28 +0900

    update logging library

--- libs/NetToolsLogger.class.php
+++ libs/NetToolsLogger.class.php
@@ -1,18 +1,28 @@
 <?php
-require_once(dirname(__FILE__).'/../log4php/Logger.php');
+require_once(dirname(__FILE__).'/../vendor/autoload.php');
+
+use Monolog\Formatter\LineFormatter;
+use Monolog\Handler\RotatingFileHandler;
+use Monolog\Level;
+use Monolog\Logger;
+
+date_default_timezone_set("Asia/Tokyo");
 
 class NetToolsLogger {
     private static $instance = null;
     private $log = null;
     private function __construct() {
-        Logger::configure(dirname(__FILE__).'/../configs/logging.xml');
-        $this->log = Logger::getLogger(__CLASS__);
+        $this->log = new Logger('net-tools');
+        $this->log_formatter = new LineFormatter("[%datetime%] %level_name%: %message% %context% %extra%\n");
+        $this->log_handler = new RotatingFileHandler(dirname(__FILE__).'/../logs/net-tools.log', 0, Level::Info, true, 0644);
+        $this->log_handler->setFormatter($this->log_formatter);
+        $this->log->pushHandler($this->log_handler);
     }
     final public static function getInstance() {
-        if ($instance === null) {
-            $instance = new self;
+        if (self::$instance === null) {
+            self::$instance = new self;
         }
-        return $instance;
+        return self::$instance;
     }
     final public function __clone() {
         throw new Exception("this instance is singleton class.");
